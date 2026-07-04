7377eba78e8389e6de429842e4cd9f53403a5429
yAraY <shaoruilu9@gmail.com>
2026-07-04 16:26:52 +0800

    bump SW cache to v5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fitness-v4';
+const CACHE = 'fitness-v5';
 const ASSETS = ['./', './index.html', './css/style.css', './js/app.js', './manifest.json'];
 
 self.addEventListener('install', e => {
