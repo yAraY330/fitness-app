8a4e60a3ca8fe14408682287413557ca902b005b
yAraY <shaoruilu9@gmail.com>
2026-07-04 23:09:22 +0800

    bump SW cache to v7 — force mobile refresh

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fitness-v6';
+const CACHE = 'fitness-v7';
 const ASSETS = ['./', './index.html', './css/style.css', './js/app.js', './manifest.json'];
 
 self.addEventListener('install', e => {
